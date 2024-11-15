50fc1dc2ec8aa14274dc93feb0c953598dea3826
cbh778899 <cbh778899@outlook.com>
2024-11-15 16:13:33 +1100

    build socket connection
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// EXPRESS SERVER
 const express = require("express");
 const {join} = require("path")
 
@@ -6,9 +7,20 @@ const app = express();
 app.use(require("body-parser").json())
 app.use(require("cors")())
 
-const router = express.Router();
+let sendSockets = [];
 
 app.use(express.static(join(__dirname, 'dist')));
+
+const router = express.Router();
+
+router.post('/api/send-command', (req, res)=>{
+	const { type, message } = req.body;
+	const send_msg = JSON.stringify({type, message})
+	sendSockets.forEach(s=>{
+		s(send_msg)
+	})
+	res.status(200).send('ok')
+})
 
 router.get("*", (req, res)=>{
     res.redirect(join(__dirname, 'dist', 'index.html'));
@@ -17,5 +29,38 @@ router.get("*", (req, res)=>{
 app.use('/', router);
 
 app.listen(3000, '0.0.0.0', () => {
-    console.log("Server is listening on port 3000!")
+    console.log("Express server is listening on port 3000!")
 })
+
+// SOCKET
+const net = require('net');
+
+const server = net.createServer((socket) => {
+	console.log('Client connected');
+
+	function sendSocket(message) {
+		socket.write(message);
+	}
+
+	sendSockets.push(sendSocket)
+
+	socket.on('data', (data) => {
+		const data_str = data.toString();
+		console.log('Received:', data_str);
+	});
+
+	socket.on('end', () => {
+		sendSockets = sendSockets.filter(e=>e!==sendSocket)
+		console.log('Client disconnected');
+	});
+
+	socket.on('error', (err) => {
+		sendSockets = sendSockets.filter(e=>e!==sendSocket)
+		console.error('Socket error:', err);
+	});
+});
+
+
+server.listen(3456, '0.0.0.0', () => {
+  	console.log(`Socket server is listening on port 3456`);
+});
