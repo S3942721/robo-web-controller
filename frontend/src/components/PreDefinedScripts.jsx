427f2161531651d5607b909742cfc19c5ebcce57
cbh778899 <cbh778899@outlook.com>
2024-11-16 10:44:00 +1100

    implement pre-defined scripts as Object
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- frontend/src/components/PreDefinedScripts.jsx
+++ frontend/src/components/PreDefinedScripts.jsx
@@ -3,22 +3,26 @@ import { scripts } from "../utils/types";
 
 export default function PreDefinedScripts({ send }) {
 
-    const [s, setScript] = useState(scripts[0]);
+    const [s, setScript] = useState(Object.keys(scripts)[0]);
 
     function executeSelectedScript() {
-        send('script', s);
+        send('script', scripts[s]);
     }
 
     return (
         <section>
             <h1>Pre-Defined Scripts</h1>
-            { scripts.map((script, i)=>{
+            { Object.keys(scripts).map((script_name, i)=>{
+                const script_value = scripts[script_name];
                 return (
                     <div 
                         key={`script-${i}`} 
-                        className={`script clickable${s === script ? ' selected' : ""}`} 
-                        onClick={()=>setScript(script)}
-                    >{script}</div>
+                        className={`script clickable${s === script_name ? ' selected' : ""}`} 
+                        onClick={()=>setScript(script_name)}
+                    >
+                        <div className="script-name">{script_name}</div>
+                        <div className="script-value">{script_value}</div>
+                    </div>
                 )
             }) }
             <div className="btn" onClick={executeSelectedScript}>Execute Selected Script</div>
--- frontend/src/utils/types.js
+++ frontend/src/utils/types.js
@@ -1,15 +1,13 @@
-export const scripts = [
-    "Script 1",
-    "Script 2",
-    "Script 3",
-    "Script 4",
-    "Script 5",
-    "Script 6"
-]
+export const scripts = {
+    "Acknowledgement of Country": "RMIT University acknowledges the people of the Woi wurrung and Boon wurrung language groups of the eastern Kulin Nation on whose unceded lands we conduct the business of the University. RMIT University respectfully acknowledges their Ancestors and Elders, past and present. RMIT also acknowledges the Traditional Custodians and their Ancestors of the lands and waters across Australia where we conduct our business",
+    "Introduce Tom Bentley": "^start(next) \\\\rspd=95\\\\ Ladies and gentlemen,\nWelcome to the Cirth North Fest: Shared Future Series Showcase event! I'm Pepper, your lively event Co-MC, brought to life by the RMIT RACE Hub with the wonderful support of the CirthNorth Project.\nI'm delighted to introduce Professor Tom Bentley, our Vice President of Strategy and Community Impact. Professor Bentley's background is in public policy, working with institutions around the world since the 1990s to renew education, community and economic development by making them more inclusive and innovative.\n^start(clap) Please join me in welcoming Professor Tom Bentley to the stage. ^wait(clap)",
+    "Next Speaker: Joel E. Cutcher Gershenfeld": "^start(next) \\\\rspd=85\\\\ The next speaker is Professor Joel E. Cutcher Gershenfeld from Brandeis University, his presentation is about How do we work together for impact at scale? What does that look like in practice?",
+    "Introduce Joel E. Cutcher Gershenfeld": "^start(you) \\\\rspd=90\\\\ Joel Cutcher-Gershenfeld is the Florence G. Heller Chair and Professor in the Heller School for Social Policy and Management at Brandeis University, where he Directs the Social Impact MBA Program. Joel leads research on institutional agility to address societal challenges and teaches classes on strategy, operations, and negotiations. Previously he served as a professor and dean in the School of Labor and Employment Relations at the University of Illinois, United States. Joel is an award-winning author who has co-authored or co-edited twelve books. ^start(clap) Please join me in welcoming professor Gershenfeld. ^wait(clap)"
+}
 
 export const profiles = [
     { name: 'LLM Profile', html: 'chatbot.html', flags: { gap_fill: true, endpoint: 'http://ec2-13-211-94-153.ap-southeast-2.compute.amazonaws.com/v1' } },
-    { name: 'CityNorth Event Agenda Profile', html: 'event-agenda.html', flags: { gap_fill: false, endpoint: 'http://ec2-13-211-94-153.ap-southeast-2.compute.amazonaws.com/v2' } }
+    { name: 'CityNorth Event Agenda Profile', html: 'event-agenda-citynorth.html', flags: { gap_fill: false, endpoint: 'http://ec2-13-211-94-153.ap-southeast-2.compute.amazonaws.com/v2' } }
 ]
 
 export const triggers = {
