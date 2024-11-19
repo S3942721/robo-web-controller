90c077c0936cec73bb7b3cb7a90e00e7b8ac4c08
cbh778899 <cbh778899@outlook.com>
2024-11-19 13:47:04 +1100

    add switch between scripts
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- frontend/src/components/PreDefinedScripts.jsx
+++ frontend/src/components/PreDefinedScripts.jsx
@@ -4,22 +4,41 @@ import { getScript } from "../utils/types";
 export default function PreDefinedScripts({ profile, send }) {
     
     const [scripts, setAllScripts] = useState({});
+    const [arrScripts, setArrScripts] = useState([]);
     const [s, setScript] = useState("");
 
     function executeSelectedScript() {
         send('script', scripts[s]);
     }
 
+
+
     useEffect(()=>{
         const all_scripts = getScript(profile)
         setAllScripts(all_scripts);
-        setScript(Object.keys(all_scripts)[0] ?? "")
+        const script_keys = Object.keys(all_scripts);
+        setScript(script_keys[0] ?? "")
+        setArrScripts(script_keys)
     }, [profile])
+
+    function switchSelect(way) {
+        let idx = arrScripts.indexOf(s)
+        if(way === 'next') {
+            if(++idx >= arrScripts.length) {
+                idx = 0;
+            }
+        } else if(way === 'last') {
+            if(--idx < 0) {
+                idx = arrScripts.length - 1;
+            }
+        }
+        setScript(arrScripts[idx])
+    }
 
     return (
         <section>
             <h1>Pre-Defined Scripts</h1>
-            { Object.keys(scripts).map((script_name, i)=>{
+            { arrScripts.map((script_name, i)=>{
                 const script_value = scripts[script_name];
                 return (
                     <div 
@@ -32,7 +51,11 @@ export default function PreDefinedScripts({ profile, send }) {
                     </div>
                 )
             }) }
-            <div className="btn" onClick={executeSelectedScript}>Execute Selected Script</div>
+            <div className="inline-btns">
+                <div className="btn" onClick={()=>switchSelect('last')}>Switch to Last Script</div>
+                <div className="btn" onClick={executeSelectedScript}>Execute Selected Script</div>
+                <div className="btn" onClick={()=>switchSelect('next')}>Switch to Next Script</div>
+            </div>
         </section>
     )
 }
