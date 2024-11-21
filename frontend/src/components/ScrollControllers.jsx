2970f48a220f992c26ffa394e01227eb80564d54
cbh778899 <cbh778899@outlook.com>
2024-11-21 03:31:21 +0000

    add new signals
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- frontend/src/components/ScrollControllers.jsx
+++ frontend/src/components/ScrollControllers.jsx
@@ -8,6 +8,12 @@ export default function ScrollControllers() {
             <ScrollBar name='Adjust Volume' initial={50} callback={vol=>{
                 requestWS('req-execute', { type: 'trigger', message: {Signal: 'Volume', Value: vol} })
             }} />
+            <ScrollBar name='Greet Face Lost Timeout' initial={3} max={5} min={0.5} step={0.5} callback={timeout=>{
+                requestWS('req-execute', { type: 'trigger', message: {Signal: 'ChangeGreetFaceLostTimeout', Value: timeout} })
+            }} />
+            <ScrollBar name='Greet Timeout' initial={1} max={10} min={1} step={0.5} callback={timeout=>{
+                requestWS('req-execute', { type: 'trigger', message: {Signal: 'ChangeGreetTimeout', Value: timeout} })
+            }} />
         </section>
     )
 }
