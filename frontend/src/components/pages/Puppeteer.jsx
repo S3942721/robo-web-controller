2f9e760eb1a66811c491c09bfc7b57f8c519fa56
Sam Griffiths <samueljfgriffiths@icloud.com>
2025-03-06 21:17:04 +1100

    Partial implementation multi robot

--- frontend/src/components/pages/Puppeteer.jsx
+++ frontend/src/components/pages/Puppeteer.jsx
@@ -1,9 +1,31 @@
+import { useState, useEffect } from "react";
 import PreDefinedScripts from "../PreDefinedScripts";
 import ScrollControllers from "../ScrollControllers";
 import MoveController from "./MoveController";
 import PagedShortCuts from "./PagedShortCuts";
 
 export default function Puppeteer() {
+    const [activeButton, setActiveButton] = useState("Bandit");
+
+    const handleButtonClick = (button) => {
+        setActiveButton(button);
+    };
+
+    useEffect(() => {
+        const root = document.documentElement;
+        if (activeButton === "Haku") {
+            root.style.setProperty('--primary-color', 'purple');
+            root.style.setProperty('--hover-color', 'rgb(128, 0, 128)');
+            root.style.setProperty('--active-color', 'rgb(75, 0, 130)');
+            root.style.setProperty('--primary-color-light','rgb(241, 221, 255)');
+        } else {
+            root.style.setProperty('--primary-color', 'dodgerblue');
+            root.style.setProperty('--hover-color', 'rgb(14, 105, 196)');
+            root.style.setProperty('--active-color', 'rgb(10, 80, 150)');
+            root.style.setProperty('--primary-color-light', '#e4f6f8');
+        }
+    }, [activeButton]);
+
     return (
         <div className={'full-screen-unscrollable'}>
             <div className={'grid-container'}>
@@ -11,7 +33,29 @@ export default function Puppeteer() {
                     <MoveController foldable={false} compact={true} profile_switcher={true}/>
                 </div>
                 <div className={'grid-item'}>
-                    <PreDefinedScripts foldable={false} compact={true} />
+                    <div className={'grid-container robot-target-scripts'}>
+                        <div className={'grid-item'}>
+                            <PreDefinedScripts foldable={false} compact={true} />
+                        </div>
+                        <div className={'grid-item'}>
+                            <div className={'button-container'}>
+                                <button 
+                                    className={`full-width ${activeButton === "Bandit" ? "active" : ""}`} 
+                                    onClick={() => handleButtonClick("Bandit")}
+                                    style={{ backgroundColor: activeButton === "Bandit" ? 'rgb(10, 80, 150)' : 'dodgerblue' }}
+                                >
+                                    Bandit
+                                </button>
+                                <button 
+                                    className={`full-width ${activeButton === "Haku" ? "active" : ""}`} 
+                                    onClick={() => handleButtonClick("Haku")}
+                                    style={{ backgroundColor: activeButton === "Haku" ? 'rgb(75, 0, 130)' : 'purple' }}
+                                >
+                                    Haku
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className={'grid-item'}>
                     <ScrollControllers foldable={false} compact={true} triggers={true} />
